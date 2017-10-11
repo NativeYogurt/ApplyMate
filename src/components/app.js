eeed166d17efc7812ca59684901eb416e70648f8
Alexander Lukens <alexander.lukens1@gmail.com>
2017-10-11 13:46:18 -0400

    Add resumeUpload.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Resume from './resumeUpload';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,7 +8,7 @@ class App extends React.Component {
     };
   }
   render() {
-    return (<div>Hello World</div>);
+    return (<Resume />);
   }
 }
 
---
+++ src/components/resumeUpload.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Resume = () => (
+  <input type="file" name="resume" accept="application/pdf" />
+);
+
+export default Resume;
