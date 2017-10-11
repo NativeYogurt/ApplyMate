b799559d8bb87187bb7aa6a3b1ca3f0693411a65
Alexander Lukens <alexander.lukens1@gmail.com>
2017-10-11 10:08:28 -0400

    configure eslinter

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (<div>Hello World</div>);
+  }
+}
+
+export default App;
