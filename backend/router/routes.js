1d10d8d821d58c7c6f4b448f85633e8c442a8811
bryang <yangxyz@gmail.com>
2017-10-12 16:53:13 -0400

    Update routes.js

--- backend/router/routes.js
+++ backend/router/routes.js
@@ -1,22 +1,12 @@
 // const express = require('express');
 const router = require('express').Router();
 const resume = require('../controllers/resume.js');
-<<<<<<< HEAD
 const userHandler = require('../controllers/userHandler');
 const job = require('../controllers/job.js');
 
 router.post('/resume', resume.uploadHandler);
 router.post('/signUp', userHandler.signUp);
 router.post('/job', job.handleJobAdd);
-=======
-const job = require('../controllers/job.js');
-// const signupHandler = require('../controllers/signupHandler');
-// const loginHandler = require('../controllers/loginHandler');
-//
-router.post('/resume', resume.uploadHandler);
-//
-router.post('/job', job.uploadHandler);
-// router.post('/login', loginHandler.handleLogin);
->>>>>>> Add webscraper
+
 
 module.exports = router;
