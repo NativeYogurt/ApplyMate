43f5383d0120cb8cc864a77ff0bb8cd4ac44cb20
paulj <jaffrepaul@gmail.com>
2017-10-13 12:38:56 -0400

    fix merge conflict

--- src/components/mainBoard.js
+++ src/components/mainBoard.js
@@ -4,7 +4,6 @@ import { browserHistory, Route, Switch } from 'react-router-dom';
 import Dashboard from './dashboard.js';
 import Resume from './resume.js';
 import Resources from './resources.js';
-import Profile from './profile.js';
 
 class Main extends React.Component {
   constructor(props) {
@@ -18,7 +17,6 @@ class Main extends React.Component {
         <Switch>
           <Route path="/home/resume" render={() => (<Resume userId={this.props.userId} />)} />
           <Route path="/home/resources" render={() => (<Resources userId={this.props.userId} />)} />
-          <Route path="/home/profile" render={() => (<Profile userId={this.props.userId} />)} />
           <Route render={() => (<Dashboard userId={this.props.userId} />)} />
         </Switch>
       </div>
@@ -27,3 +25,5 @@ class Main extends React.Component {
 }
 
 export default Main;
+
+// <Route path="/home/profile" render={() => (<Profile userId={this.props.userId} />)} />
