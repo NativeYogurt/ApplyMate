8f6bce77b2227e9567afe8c8ba8c1e84d42e1db8
Alexander Lukens <alexander.lukens1@gmail.com>
2017-10-13 11:49:17 -0400

    Extract skills from job posting

--- backend/controllers/job.js
+++ backend/controllers/job.js
@@ -5,35 +5,37 @@ const parser = require('body-parser');
 const sequelize = require('../db/db.js');
 const Users = require('../models/User.js');
 const SavedJobs = require('../models/SavedJobs.js');
+const extract = require('../utilities/extractSkills.js');
 
 const Xray = require('x-ray');
-const striptags = require('striptags');
+
 const x = Xray();
 
 exports.handleJobAdd = (req, res) => {
   console.log('req', req.body);
-  let reqSkills = [];
-  x(req.body.url, (['ol'],['ul'], ['li']))((err, data) => {
-    reqSkills = data;
-    const newJob = {
-      company: req.body.company,
-      jobTitle: req.body.jobTitle,
-      description: req.body.description,
-      url: req.body.url,
-      skills: reqSkills,
-      userId: req.body.userId,
-    };
-    SavedJobs
-      .build(newJob)
-      .save()
-      .then((data) => {
-        console.log('saved job', data);
-        res.send(data);
-      })
-      .catch((error) => {
-        throw error;
+  let text;
+  x(req.body.url, (['ol'], ['ul'], ['li']))((err, data) => {
+    text = data.join(' ');
+    extract.extractSkills(text)
+      .then((skills) => {
+        const newJob = {
+          company: req.body.company,
+          jobTitle: req.body.jobTitle,
+          description: req.body.description,
+          url: req.body.url,
+          skills,
+          userId: req.body.userId,
+        };
+        SavedJobs
+          .build(newJob)
+          .save()
+          .then((job) => {
+            console.log('saved job', job);
+            res.send(job);
+          })
+          .catch((error) => {
+            throw error;
+          });
       });
   });
-
-
 };
