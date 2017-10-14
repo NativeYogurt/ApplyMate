9250a1a942579dc92d46d56724ad28ae25816259
Kenneth Tso <iamkennytso@gmail.com>
2017-10-14 17:09:15 -0400

    (feat) github login

--- backend/controllers/userHandler.js
+++ backend/controllers/userHandler.js
@@ -8,5 +8,17 @@ exports.signUp = (req, res) => {
     lastName: req.body.data.lastName,
     email: req.body.data.email,
   })
-  console.log(User);
+  .then(user => console.log('heyitssignup', user))
+};
+exports.scanforUser = (req, res) => {
+  console.log('userhand', req.body.data.email);
+  User.findAll({
+    where: {
+      email: req.body.data.email,
+    },
+  })
+    .then((user) => {
+      res.send(user);
+      console.log('kenny', user);
+    });
 };
--- backend/router/routes.js
+++ backend/router/routes.js
@@ -3,14 +3,12 @@ const router = require('express').Router();
 const resume = require('../controllers/resume.js');
 const userHandler = require('../controllers/userHandler');
 const job = require('../controllers/job.js');
-const user = require('../controllers/user');
 const comparison = require('../controllers/comparison.js');
 
 router.post('/resume', resume.uploadHandler);
-router.post('/signUp', userHandler.signUp);
 router.post('/job', job.handleJobAdd);
 router.get('/comparison', comparison.getComparison);
-router.post('/findUser', user.handleUserFind);
-router.put('/updateUser', user.handleUpdateUser);
+router.post('/signUp', userHandler.signUp);
+router.post('/scanForUser', userHandler.scanforUser);
 
 module.exports = router;
