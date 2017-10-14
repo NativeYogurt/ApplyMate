f067d2c9c1acb4194d1b184890f81ab2cc1cb0ca
BryanWang <yangxyz@gmail.com>
2017-10-14 16:03:41 -0400

    Adjust user profile page

--- src/components/mainBoard.js
+++ src/components/mainBoard.js
@@ -7,17 +7,48 @@ import Resume from './resume';
 import Resources from './resources';
 import Profile from './Profile';
 
-function Main(props) {
-  return (
-    <div>
-      <Switch>
-        <Route path="/home/resume" render={() => (<Resume userId={props.userId} />)} />
-        <Route path="/home/resources" render={() => (<Resources userId={props.userId} />)} />
-        <Route path="/home/profile" render={() => (<Profile userId={props.userId} />)} />
-        <Route render={() => (<Dashboard userId={props.userId} />)} />
-      </Switch>
-    </div>
-  );
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+    };
+  }
+  componentDidMount() {
+    fetch('/api/findUser', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userId: this.props.userId }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.setState({
+          firstName: data.firstName,
+          lastName: data.lastName,
+          email: data.email,
+        });
+      })
+      .catch(error => console.log('error getting data'));
+  }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/home/resume" render={() => (<Resume userId={this.props.userId} />)} />
+          <Route path="/home/resources" render={() => (<Resources userId={this.props.userId} />)} />
+          <Route path="/home/profile" render={() => (<Profile userEmail={this.state.email} userFirstName={this.state.firstName} userLastName={this.state.lastName} userId={this.props.userId} />)} />
+          <Route render={() => (<Dashboard userId={this.props.userId} />)} />
+        </Switch>
+      </div>
+    );
+  }
 }
 Main.propTypes = {
   userId: PropTypes.string.isRequired,
