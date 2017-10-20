73a02d780852d0189a731e2d56bfcc07d2597b35
paulj <jaffrepaul@gmail.com>
2017-10-20 10:11:45 -0400

    fixed merge conflict

--- src/components/UploadedResume.js
+++ src/components/UploadedResume.js
@@ -75,6 +75,18 @@ class UploadedResume extends React.Component {
     );
   }
 }
+<<<<<<< HEAD
+=======
+
+// const UploadedResume = ({ userResume }) => {
+//   return (
+//     <div>
+//       {userResume ? <PDF
+//         file={userResume} /> : <div>Add your resume to compare your skills!</div>}
+//     </div>
+//   );
+// };
+>>>>>>> fixed merge conflict
 
 UploadedResume.propTypes = {
   userResume: PropTypes.string.isRequired,
