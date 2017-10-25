667b1cca592dd63c4a4090e437bb6187b03fe2a5
paulj <jaffrepaul@gmail.com>
2017-10-24 20:04:36 -0400

    sending email working. need to hide creds & plan function invokation

--- backend/utilities/emailer.js
+++ backend/utilities/emailer.js
@@ -1,28 +1,33 @@
 import nodemailer from 'nodemailer';
 
-const emailer = () => {
+nodemailer.createTestAccount((err, account) => {
+  // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'applymatebot@gmail.com',
-      pass: 'password1111',
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
     },
   });
 
   const mailOptions = {
-    from: 'applymatebot@gmail.com',
-    to: 'jaffrepaul@gmail.com',
-    subject: 'Interview Reminder!',
-    text: 'You have an interview tomorrow!',
+    from: '"ApplyMate" <applymatebot@gmail.com>', // sender address
+    to: 'jaffrepaul@gmail.com', // list of receivers
+    subject: 'You have an interview! 🔥🔥', // Subject line
+    text: 'You have an interview tomorrow!', // plain text body
+    html: '<p>You have an interview tomorrow with [...].</p><p>Good luck!</p>💋<p>-ApplyMate</p>', // html body
   };
 
+  // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
+      return console.log(error);
     }
-  });
-};
+    console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-exports.emailer = emailer;
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  });
+});
