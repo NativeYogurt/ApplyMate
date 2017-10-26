348434d7374e3abe08c3ebdeb4397185f8665597
paulj <jaffrepaul@gmail.com>
2017-10-26 17:21:41 -0400

    file cleanup

--- src/components/analytics/analytics.js
+++ src/components/analytics/analytics.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Bar, Line, Pie, Doughnut, Radar, Polar } from 'react-chartjs-2';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-import GithubSkills from '../profile/githubSkills';
+import GithubSkills from '../profile/github-skills';
 
 import JobStatus from './jobstatus';
 import Applications from './applications';
