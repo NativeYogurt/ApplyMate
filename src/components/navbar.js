3fec90de8d208ee4c48e82fb68665ad0a54ffecb
paulj <jaffrepaul@gmail.com>
2017-10-28 15:59:06 -0400

    fixed nav bug

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -29,21 +29,19 @@ class Nav extends React.Component {
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">ApplyMate</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/home/dashboard"><i className="large material-icons">dashboard</i></Link></li>
-            <li><Link to="/home/searchjobs"><i className="large material-icons">search</i></Link></li>
-            <li><Link to="/home/resources"><i className="large material-icons">folder_open</i></Link></li>
-            <li><Link to="/home/tasks"><i className="large material-icons">playlist_add_check</i></Link></li>
-            <li><Link to="/home/analytics"><i className="large material-icons">timeline</i></Link></li>
-            <li><a className="dropdown-button" data-activates="dropdown1"><i id="menu-arrow"  className="material-icons right">arrow_drop_down</i><i className="large material-icons">account_circle</i></a></li>
+            <li><Link to="/home/dashboard">Dashboard</Link></li>
+            <li><Link to="/home/searchjobs">Search Jobs</Link></li>
+            <li><Link to="/home/resources">Resources</Link></li>
+            <li><Link to="/home/tasks">Tasks</Link></li>
+            <li><Link to="/home/analytics">Analytics</Link></li>
+            <li><a className="dropdown-button" data-activates="dropdown1"><i className="large material-icons">account_circle</i></a></li>
           </ul>
         </div>
-
         <ul id="dropdown1" className="dropdown-content">
           <li><Link to="/home/profile"><i className="large material-icons">settings</i> Profile</Link></li>
           <li className="divider" />
           <li onClick={e => this.handleSignOut(e)}><Link to="/"><i className="large material-icons">exit_to_app</i>Sign Out</Link></li>
         </ul>
-
       </nav>
     );
   }
