62077560cdfc8067d3de57d7c8735c95427ae4c9
BryanWang <yangxyz@gmail.com>
2017-10-30 15:11:12 -0400

    Fix profile on navbar and add style for active navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {Dropdown, Button, Navbar, NavItem } from 'react-materialize';
+import { Dropdown, Button, Navbar, NavItem } from 'react-materialize';
 import Auth from './Auth';
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      active: 'dashboard',
     };
   }
   handleTest(e) {
@@ -23,31 +24,60 @@ class Nav extends React.Component {
       }
     });
   }
+  setActive(item) {
+    this.setState({
+      active: item,
+    });
+  }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">ApplyMate</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/home/dashboard">Dashboard</Link></li>
-            <li><Link to="/home/searchjobs">Search Jobs</Link></li>
-            <li><Link to="/home/resources">Resources</Link></li>
-            <li><Link to="/home/tasks">Tasks</Link></li>
-            <li><Link to="/home/analytics">Analytics</Link></li>
-            <li><Dropdown trigger={
-		              <div><i className="large material-icons">account_circle</i></div>}>
-	                <NavItem><Link to="/home/profile"><i className="large material-icons">settings</i> Profile</Link></NavItem>
-	                <NavItem divider />
-	                <NavItem onClick={e => this.handleSignOut(e)}><Link to="/"><i className="large material-icons">exit_to_app</i>Sign Out</Link></NavItem>
-                </Dropdown>
+            <li
+              className={this.state.active === 'dashboard' ? 'active' : ''}
+              onClick={() => {this.setActive('dashboard')}}>
+              <Link to="/home/dashboard">Dashboard</Link>
+            </li>
+            <li
+              className={this.state.active === 'search' ? 'active' : ''}
+              onClick={() => {this.setActive('search')}}>
+              <Link to="/home/searchjobs">Search Jobs</Link>
+            </li>
+            <li
+              className={this.state.active === 'resources' ? 'active' : ''}
+              onClick={() => {this.setActive('resources')}}>
+              <Link to="/home/resources">Resources</Link>
+            </li>
+            <li
+              className={this.state.active === 'tasks' ? 'active' : ''}
+              onClick={() => {this.setActive('tasks')}}>
+              <Link to="/home/tasks">Tasks</Link>
+            </li>
+            <li
+              className={this.state.active === 'analytics' ? 'active' : ''}
+              onClick={() => {this.setActive('analytics')}}>
+              <Link to="/home/analytics">Analytics</Link>
+            </li>
+            <li
+              className={this.state.active === 'profile' ? 'active' : ''}
+              onClick={() => {this.setActive('profile')}}>
+              <Dropdown
+                trigger={<a><i className="large material-icons">account_circle</i></a>}
+              >
+                <li><Link to="/home/profile"><i className="large material-icons">settings</i> Profile</Link></li>
+                <li onClick={e => this.handleSignOut(e)}><Link to="/"><i className="large material-icons">exit_to_app</i>Sign Out</Link></li>
+              </Dropdown>
             </li>
           </ul>
         </div>
-
       </nav>
     );
   }
 }
+
+
 Navbar.propTypes = {
   setUser: PropTypes.func.isRequired,
 };
