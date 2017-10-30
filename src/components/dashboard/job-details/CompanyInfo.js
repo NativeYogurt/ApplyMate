12e0b79f0f5d113c2aa41f0df93482fd0a7a2b5d
Kenneth Tso <iamkennytso@gmail.com>
2017-10-30 09:52:28 -0400

    is reverts commit ce9a9412167a7d04530428307ef93c8f62ff6e8e.

--- src/components/dashboard/job-details/CompanyInfo.js
+++ src/components/dashboard/job-details/CompanyInfo.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import BBB from '../company-info/BBB.js';
-import Glassdoor from '../company-info/Glassdoor.js';
-import EDGAR from '../company-info/edgar.js';
-import Twitter from '../company-info/twitter.js';
+import BBB from '../company-info/BBB.js'
+import Glassdoor from '../company-info/Glassdoor.js'
+import EDGAR from '../company-info/edgar.js'
+import Twitter from '../company-info/twitter.js'
 import TwitterBanner from '../company-info/twitterBanner.js'
-;
 class CompanyInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       companyName: this.props.company,
       twitterData: [],
-      twitterPic: '/imgs/TempHeader.jpg',
+      twitterPic: '/imgs/TempHeader.jpg'
     };
     this.twitterApiCall = this.twitterApiCall.bind(this);
   }
@@ -25,13 +24,10 @@ class CompanyInfo extends React.Component {
   twitterApiCall() {
     axios.post('/api/Twitter', { searchTerm: this.state.companyName })
       .then(data => {
-        console.log(data);
-        if (data.data[0].url !== 'www.twitter.com/IAmKennyTso') {
-          this.setState({
-            twitterData: data.data,
-            twitterPic: data.data[0].pic || this.state.twitterPic,
-          });
-        }
+        this.setState({
+          twitterData: data.data,
+          twitterPic: data.data[0].pic,
+        });
       });
   }
 
