73dff4160cec3793b77f44650ba54f07b6108dab
paulj <jaffrepaul@gmail.com>
2017-11-01 18:44:17 -0400

    updated conditional logic to account for only github analytics

--- src/components/analytics/analytics.js
+++ src/components/analytics/analytics.js
@@ -9,29 +9,38 @@ import Interviews from './interviews';
 
 const Analytics = (props) => {
   const hasJobAnalytics = props.savedJobs.length;
-  const hasGithubAnalytics = Object.keys(props.githubSkills).length !== 0;
+  const hasGithubAnalytics = Object.keys(props.githubSkills).length;
 
-  return hasJobAnalytics ?
-    (
-      <div>
-        <JobStatus savedJobs={props.savedJobs} />
-        <Applications savedJobs={props.savedJobs} />
-        {Object.keys(props.githubSkills).length ?
-          <GithubSkills
-            githubSkills={props.githubSkills}
-          /> : null }
-      </div>
-    ) :
-      <div className="empty-state">
-        <Row className="center">
-          <Col s={12}>
-            <Icon medium>insert_chart</Icon>
-            <p>Add Jobs To Your Dashboard To Track Your Progress!</p>
-          </Col>
-        </Row>
-      </div>;
+  let charts;
+  if (hasJobAnalytics && hasGithubAnalytics) {
+    charts =
+    (<div>
+      <JobStatus savedJobs={props.savedJobs} />
+      <Applications savedJobs={props.savedJobs} />
+      <GithubSkills githubSkills={props.githubSkills} />
+     </div>);
+  } else if (hasJobAnalytics) {
+    charts =
+     (<div>
+       <JobStatus savedJobs={props.savedJobs} />
+       <Applications savedJobs={props.savedJobs} />
+      </div>);
+  } else if (hasGithubAnalytics) {
+    charts = <GithubSkills githubSkills={props.githubSkills} />;
+  } else {
+    charts =
+    (<div className="empty-state">
+      <Row className="center">
+        <Col s={12}>
+          <Icon medium>insert_chart</Icon>
+          <p>Add Jobs To Your Dashboard To Track Your Progress!</p>
+        </Col>
+      </Row>
+     </div>);
+  }
+  return (
+    <div>{charts}</div>
+  );
 };
 
 export default Analytics;
-
-// <Interviews userId={props.userId} />
