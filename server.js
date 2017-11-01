894469c936690998bc46c506f21f04fde8b9c58a
Alexander Lukens <alexander.lukens1@gmail.com>
2017-11-01 11:56:39 -0400

    Add ENV variable to server

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(webpackDevMiddleware(compiler, {
   historyApiFallback: true,
 }));
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
