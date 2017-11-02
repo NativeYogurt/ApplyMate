e30b5d929a32779b7a10afb43529f9b5c6d50366
BryanWang <yangxyz@gmail.com>
2017-11-02 14:41:56 -0400

    Fix card width

--- src/components/dashboard/job-details/JobNavBar.js
+++ src/components/dashboard/job-details/JobNavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Row, Col } from 'react-materialize';
 
 class JobNavbar extends React.Component {
   constructor(props) {
